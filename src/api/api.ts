af6bc99409977521a2394b86896da31308dbffc8
Emilio Suárez <91371538+emisua@users.noreply.github.com>
2024-01-27 14:54:10 +0100

    movie list as server component

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,35 +1,36 @@
-import type { Api_Response } from "../types/types";
+import type { Api_Response } from '../types/types'
 
-const bearerKey: string = import.meta.env.PUBLIC_BEARER_KEY;
+const bearerKey: string = import.meta.env.BEARER_KEY
 
-const PREFIX_URL = "https://api.themoviedb.org/3";
-const LANGUAGE = "?language=es-ES";
-const PAGE = "?page=1";
+const PREFIX_URL = 'https://api.themoviedb.org/3'
+const LANGUAGE = '?language=es-ES'
+const PAGE = '?page=1'
 
 const options = {
-  method: "GET",
+  method: 'GET',
   headers: {
-    accept: "application/json",
+    accept: 'application/json',
     Authorization: `Bearer ${bearerKey}`,
   },
-};
+}
 
 export async function getMovies() {
   const response = await fetch(
-    `${PREFIX_URL}/movie/upcoming${LANGUAGE}${PAGE}`,
+    `${PREFIX_URL}/movie/upcoming${LANGUAGE}&${PAGE}`,
     options
-  );
+  )
 
-  const movies: Api_Response = await response.json();
-  return movies?.results;
+  const movies: Api_Response = await response.json()
+  console.log(movies)
+  return movies?.results
 }
 
 export async function getMovieByTitle(name: string) {
   const response = await fetch(
     `${PREFIX_URL}/search/movie?query=${name}&${LANGUAGE}${PAGE}`,
     options
-  );
+  )
 
-  const movies: Api_Response = await response.json();
-  return movies?.results;
+  const movies: Api_Response = await response.json()
+  return movies?.results
 }
--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,25 +1,30 @@
-import { useEffect } from "react";
-import { getMovies } from "../api/api";
-import { useMoviesStore } from "../store/store";
-import MovieCard from "./MovieCard";
+//import { useEffect } from 'react'
+//import { getMovies } from '../api/api'
+//import { useMoviesStore } from '../store/store'
+import MovieCard from './MovieCard'
+import type { Movie } from '../types/types'
 
-const MovieList = () => {
-  const { setMovieList } = useMoviesStore((state) => state);
-  const { movieList } = useMoviesStore((state) => state);
+interface Props {
+  movieList: Movie[]
+}
 
-  console.log("entro");
-
-  useEffect(() => {
-    getMovies().then((movies) => setMovieList(movies));
-  }, []);
+const MovieList = ({ movieList }: Props) => {
+  //const { setMovieList } = useMoviesStore((state) => state)
+  // const { movieList } = useMoviesStore((state) => state)
 
   return (
-    <ul role="list" className="grid grid-cols-4 gap-4">
+    <ul
+      role='list'
+      className='grid grid-cols-4 gap-4'
+    >
       {movieList?.map((movie) => (
-        <MovieCard movie={movie} key={movie.id} />
+        <MovieCard
+          movie={movie}
+          key={movie.id}
+        />
       ))}
     </ul>
-  );
-};
+  )
+}
 
-export default MovieList;
+export default MovieList
