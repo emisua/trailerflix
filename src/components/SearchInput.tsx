a14a9d45f4a35acb0d59be02abf4d996e91f37f0
emisua <91371538+emisua@users.noreply.github.com>
2024-01-28 13:45:50 +0100

    movie details

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,35 +1,36 @@
-import { useState, type FormEvent } from 'react'
+import { useState, type FormEvent } from "react";
+import SearchIcon from "../icons/SearchIcon";
 
 const SearchInput = () => {
-  const [movieTitle, setMovieTitle] = useState<string>('')
+  const [movieTitle, setMovieTitle] = useState<string>("");
 
   function searchMovie(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault()
-    window.location.href = `/search/${movieTitle}`
+    e.preventDefault();
+    window.location.href = `/search/${movieTitle}`;
   }
 
   return (
     <form
-      className='flex gap-2'
+      className="flex"
       onSubmit={(e) => searchMovie(e)}
       //action={`/search?=${movieTitle}`}
     >
       <input
-        type='text'
-        placeholder='Buscar'
+        type="text"
+        placeholder="Search for a movie"
         onChange={(e) => {
-          setMovieTitle(e.target.value)
+          setMovieTitle(e.target.value);
         }}
-        className='rounded p-2 bg-neutral-700 backdrop-blur-lg'
+        className="text-neutral-300 text-opacity-60 rounded-tl rounded-bl px-2 py-1 bg-neutral-700/90 backdrop-blur-sm text-sm "
       />
       <button
-        className='bg-red-500 p-2 text-neutral-300 rounded'
-        type='submit'
+        className="bg-red-500/30 backdrop-blur-sm px-2 py-1 text-neutral-300 rounded-tr rounded-br text-sm"
+        type="submit"
       >
-        Buscar
+        <SearchIcon className="w-4 h-4" />
       </button>
     </form>
-  )
-}
+  );
+};
 
-export default SearchInput
+export default SearchInput;
