66b5315c34517324b8c211f910f7fc2da6870375
Emilio Suárez <91371538+emisua@users.noreply.github.com>
2024-02-09 19:31:10 +0100

    fix format

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,27 @@
-export function formatBudget(budget: Number) {
+export function formatBudget(budget: string) {
+  // Convierte el presupuesto a una cadena de texto
   const stringBudget = budget.toString()
+
+  // Determina la longitud del presupuesto
   const budgetLength = stringBudget.length
-  if (budgetLength === 9) return stringBudget?.slice(0, 3) + 'M'
-  if (budgetLength === 8) return stringBudget?.slice(0, 2) + 'M'
-  if (budgetLength === 7) return stringBudget?.slice(0, 1) + 'M'
-  if (budgetLength < 7 && budgetLength > 3)
-    return stringBudget?.slice(0, 1) + 'K'
-  return stringBudget
+
+  // Define las opciones para el formato de moneda
+  const options = {
+    style: 'currency',
+    currency: 'EUR', // Cambia 'EUR' por el código de moneda deseado
+  }
+
+  // Aplica el formato basado en la longitud del presupuesto
+  let formattedBudget
+  if (budgetLength >= 7) {
+    formattedBudget = stringBudget.slice(0, -6) + 'M' // Millones
+  } else if (budgetLength >= 4) {
+    formattedBudget = stringBudget.slice(0, -3) + 'K' // Miles
+  } else {
+    formattedBudget = parseFloat(stringBudget).toLocaleString('es-ES', options) // Menos de 1000
+  }
+
+  // Regresa el valor formateado
+  console.log(formattedBudget)
+  return `${formattedBudget} €`
 }
